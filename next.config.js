58a6ef93508e31fe72915dfce7df1dfb0718b013
Abhijeet Singh <abhijeet.singh@razorpay.com>
2023-01-03 21:11:58 +0530

    chore: upgrade next 12 to 13

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const withTM = require("next-transpile-modules")(["hazel-ui"]);
 
+/** @type {import('next').NextConfig} */
 module.exports = withTM({
   // basePath: "/blog", // when deploying to sub-path
+  compiler: {
+    styledComponents: true,
+  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
