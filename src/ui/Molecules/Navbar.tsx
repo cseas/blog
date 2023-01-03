192635290e5d443ade20456c3ccd26ba5e135295
Abhijeet Singh <abhijeet.singh@razorpay.com>
2023-01-03 21:37:49 +0530

    refactor: link doesn't need anchor tag as child anymore

--- src/ui/Molecules/Navbar.tsx
+++ src/ui/Molecules/Navbar.tsx
@@ -1,35 +1,25 @@
 import Link from "next/link";
-import { HTMLProps } from "react";
 
 export function Navbar() {
+  const pages = [
+    { href: "/", label: "Home" },
+    { href: "/posts", label: "Posts" },
+    { href: "/learn", label: "Learn" },
+    { href: "/links", label: "Links" },
+  ];
+
   return (
     <div className="sticky top-0 z-10 border-b border-mauve-7 bg-slate-50/75 backdrop-blur-xl backdrop-saturate-150 dark:bg-slate-900/75">
       <div className="my-0 mx-auto flex max-w-xl justify-between px-5 py-2.5">
-        <Link href="/" passHref>
-          <Label>Home</Label>
-        </Link>
-
-        <Link href="/posts" passHref>
-          <Label>Posts</Label>
-        </Link>
-
-        <Link href="/learn" passHref>
-          <Label>Learn</Label>
-        </Link>
-
-        <Link href="/links" passHref>
-          <Label>Links</Label>
-        </Link>
+        {pages.map(({ href, label }) => (
+          <Link
+            href={href}
+            className="text-mauve-11 transition-colors duration-150 visited:text-mauve-11 hover:text-purple-11 hover:no-underline"
+          >
+            {label}
+          </Link>
+        ))}
       </div>
     </div>
-  );
-}
-
-function Label(props: HTMLProps<HTMLAnchorElement>) {
-  return (
-    <a
-      className="text-mauve-11 transition-colors duration-150 visited:text-mauve-11 hover:text-purple-11 hover:no-underline"
-      {...props}
-    ></a>
   );
 }
