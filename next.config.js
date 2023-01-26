37e1a66a03795cc71354884548729f02a0d3f3b5
Abhijeet Singh <abhijeet.singh@razorpay.com>
2023-01-26 18:43:08 +0530

    feat: enable app directory

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
-
 module.exports = {
   // basePath: "/blog", // when deploying to sub-path
 
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
+  },
+
+  experimental: {
+    appDir: true,
   },
 };
